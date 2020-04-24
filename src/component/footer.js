3f25c48dec5e752de7abc51f85149c0932809eab
hoangvu <map1473@gmail.com>
2020-04-24 21:48:39 +0700

    * add footer

--- src/component/footer.js
+++ src/component/footer.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Layout, Card } from "antd";
 
+const Footer = Layout.Footer;
+const footerZiim = () => {
+	return (
+		<Footer>
+			<Card>
+                <h4>How Ziim works</h4>
+            </Card>
+		</Footer>
+	);
+};
 
-const footer = () => {
-    return
-    <View>
-
-    </View>
-}
+export default footerZiim;
